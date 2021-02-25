c8d76b71251d6883a14d6ea1c135cdf3ea29fb95
li lei <ruojianll@gmail.com>
2021-02-25 19:37:14 +0800

    A

--- build/prefab/Primordial8.d.ts
+++
@@ -1,4 +0,0 @@
-import Trigram from '../gram/trigram';
-export declare const List: [Trigram, Trigram, Trigram, Trigram, Trigram, Trigram, Trigram, Trigram];
-export declare const Matrix: [[Trigram, Trigram, Trigram], [Trigram, null, Trigram], [Trigram, Trigram, Trigram]];
-//# sourceMappingURL=primordial8.d.ts.map
